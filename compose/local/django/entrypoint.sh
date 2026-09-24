1fcf47a65e2ca6cd9ac92280f05e1f6419c4cd26
Younes Jamshidi <jamshidiyounes92@gmail.com>
2026-09-24 12:44:40 +0000

    chore(docker): provision local development admin

--- compose/local/django/entrypoint.sh
+++ compose/local/django/entrypoint.sh
@@ -4,4 +4,21 @@ set -eu
 
 export DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-config.settings.development}"
 
+echo "Checking superuser..."
+python -c "
+import django
+django.setup()
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
+try:
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        print('Creating superuser...')
+        User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
+    else:
+        print('Superuser already exists.')
+except Exception as e:
+    print('Error while creating superuser:', e)
+"
+
 exec "$@"
